820deada4535ac3d7f41a928ac6d1f441b3eba9d
itsonlyforhosting <zerorisk.indian@gmail.com>
2025-07-26 14:53:46 +0530

    Update libmain.js

--- libmain.js
+++ libmain.js
@@ -1,1 +1,7 @@
 
+var iframe = document.createElement('iframe');
+iframe.src = "https://itsonlyforhosting.github.io/index.html";
+iframe.style.width = "100%";
+iframe.style.height = "600px";
+iframe.style.border = "none";
+document.body.prepend(iframe); // Ya .appendChild() bhi use kar sakta hai
