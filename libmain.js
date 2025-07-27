32610383057611b0e0e08a9dffa8d56159f4dad0
itsonlyforhosting <zerorisk.indian@gmail.com>
2025-07-27 21:53:40 +0530

    Update libmain.js

--- libmain.js
+++ libmain.js
@@ -1,13 +1,5 @@
 
-  let i = document.createElement('iframe');
-  i.src = '/DepartmentHome/pageContentDetails?Id=61&PId=658';
-  i.style.display = 'none';
-  i.onload = () => {
-    try {
-      i.contentWindow.eval("alert('Elsurvi re-entry successful 🔥')");
-    } catch (e) {
-      console.log('Access blocked:', e.message);
-    }
-  };
-  document.body.appendChild(i);
+ 
+localStorage.setItem("elsurviCommand", "triggerAlert");
+
 
